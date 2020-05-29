0544c866ecb2ceaa0326121d88530a596fb08fa5
Nguyen Huu Duc <duc.nh@softworld.jp>
2020-05-29 15:51:24 +0700

    Add innit prj

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+interface IRoute {
+  link: string[];
+  title: string;
+  text: string;
+}
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+  routes: IRoute[] = [
+    {
+      link: ['/auth', 'login'],
+      text: 'Login page',
+      title: ''
+    },
+    {
+      link: ['/auth', 'registration'],
+      text: 'Registration page',
+      title: ''
+    }
+  ];
+  constructor() {}
+}
